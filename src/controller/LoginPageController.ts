65645dbf917efd000bf472ccb553c2a1fe5ea514
Le Hoang Long <le.hoang.long@outlook.com>
2021-02-26 05:24:02 +0800

    Reload page when login, log out and sign up

--- src/controller/LoginPageController.ts
+++ src/controller/LoginPageController.ts
@@ -9,10 +9,12 @@ export class LoginPageController {
 
     async onLogin(username: string, password: string) {
         await this.dispatcher.dispatchLoginAndGetUserInfo(username, password);
+        window.location.href = '/'; //reload the page to reset the cookie
     }
 
     async onSignup(username: string, password: string) {
         await this.dispatcher.dispatchSignUpAndGetUserInfo(username, password);
+        window.location.href = '/'; //reload the page to reset the cookie
     }
 
     onUserStateChanged(userState: UserState) {
--- src/controller/ProfileBarController.ts
+++ src/controller/ProfileBarController.ts
@@ -11,5 +11,6 @@ export class ProfileBarController {
     onLogout() {
         cookie.remove('jwt');
         this.userDispatcher.dispatchLogout();
+        window.location.href = '/';
     }
 }
